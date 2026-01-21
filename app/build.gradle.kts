083e9ccede7571f637b287ab68ba15691d9b6b66
Seth <sethlaurman@gmail.com>
2026-01-21 14:44:56 +0100

    more stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
 }
 
 android {
@@ -16,7 +16,7 @@ android {
     }
 
     buildFeatures {
-        viewBinding = true
+        dataBinding = true
     }
 
     compileOptions {
@@ -29,23 +29,12 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.3.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-
-    // Media3 (ExoPlayer replacement)
+    implementation("androidx.constraintlayout:constraintlayout:2.3.1") // let op 2.3.1 ipv 2.3.0
     implementation("androidx.media3:media3-exoplayer:1.2.2")
     implementation("androidx.media3:media3-ui:1.2.2")
-
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,13 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
+
 
 rootProject.name = "MyApplication4"
 include(":app")
